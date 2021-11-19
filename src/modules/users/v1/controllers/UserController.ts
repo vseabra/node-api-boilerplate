7b0721be0f5a1c8c6eb618b32afb5cd0c569b5d3
Vinícius Seabra <vseabra@protonmail.com>
2021-11-19 14:14:07 -0300

    remove swagger docs from users route

--- src/modules/users/v1/controllers/UserController.ts
+++ src/modules/users/v1/controllers/UserController.ts
@@ -25,24 +25,6 @@ import { UserValidator } from '../middlewares/UserValidator';
 
 @Controller(EnumEndpoints.USER_V1)
 export class UserController extends BaseController {
-    /**
-     * @swagger
-     * /v1/user:
-     *   get:
-     *     summary: Lista os usuários
-     *     tags: [Users]
-     *     consumes:
-     *       - application/json
-     *     produces:
-     *       - application/json
-     *     parameters:
-     *       - $ref: '#/components/parameters/listPageRef'
-     *       - $ref: '#/components/parameters/listSizeRef'
-     *       - $ref: '#/components/parameters/listOrderRef'
-     *       - $ref: '#/components/parameters/listOrderByRef'
-     *     responses:
-     *       $ref: '#/components/responses/baseResponse'
-     */
     @Get()
     @PublicRoute()
     public async get(req: Request, res: Response): Promise<void> {
@@ -51,25 +33,6 @@ export class UserController extends BaseController {
         RouteResponse.success({ rows, count }, res);
     }
 
-    /**
-     * @swagger
-     * /v1/user/{userId}:
-     *   get:
-     *     summary: Retorna informações de um usuário
-     *     tags: [Users]
-     *     consumes:
-     *       - application/json
-     *     produces:
-     *       - application/json
-     *     parameters:
-     *       - in: path
-     *         name: userId
-     *         schema:
-     *           type: string
-     *         required: true
-     *     responses:
-     *       $ref: '#/components/responses/baseResponse'
-     */
     @Get('/:id')
     @PublicRoute()
     @Middlewares(UserValidator.onlyId())
@@ -77,31 +40,6 @@ export class UserController extends BaseController {
         RouteResponse.success({ ...req.body.userRef }, res);
     }
 
-    /**
-     * @swagger
-     * /v1/user:
-     *   post:
-     *     summary: Cadastra um usuário
-     *     tags: [Users]
-     *     consumes:
-     *       - application/json
-     *     produces:
-     *       - application/json
-     *     requestBody:
-     *       content:
-     *         application/json:
-     *           schema:
-     *             type: object
-     *             example:
-     *               name: userName
-     *             required:
-     *               - name
-     *             properties:
-     *               name:
-     *                 type: string
-     *     responses:
-     *       $ref: '#/components/responses/baseCreate'
-     */
     @Post()
     @PublicRoute()
     @Middlewares(UserValidator.post())
@@ -115,35 +53,6 @@ export class UserController extends BaseController {
         RouteResponse.successCreate(res);
     }
 
-    /**
-     * @swagger
-     * /v1/user:
-     *   put:
-     *     summary: Altera um usuário
-     *     tags: [Users]
-     *     consumes:
-     *       - application/json
-     *     produces:
-     *       - application/json
-     *     requestBody:
-     *       content:
-     *         application/json:
-     *           schema:
-     *             type: object
-     *             example:
-     *               id: userId
-     *               name: userName
-     *             required:
-     *               - id
-     *               - name
-     *             properties:
-     *               id:
-     *                 type: string
-     *               name:
-     *                 type: string
-     *     responses:
-     *       $ref: '#/components/responses/baseEmpty'
-     */
     @Put()
     @PublicRoute()
     @Middlewares(UserValidator.put())
@@ -157,25 +66,6 @@ export class UserController extends BaseController {
         RouteResponse.successEmpty(res);
     }
 
-    /**
-     * @swagger
-     * /v1/user/{userId}:
-     *   delete:
-     *     summary: Apaga um usuário definitivamente
-     *     tags: [Users]
-     *     consumes:
-     *       - application/json
-     *     produces:
-     *       - application/json
-     *     parameters:
-     *       - in: path
-     *         name: userId
-     *         schema:
-     *           type: string
-     *         required: true
-     *     responses:
-     *       $ref: '#/components/responses/baseResponse'
-     */
     @Delete('/:id')
     @PublicRoute()
     @Middlewares(UserValidator.onlyId())
